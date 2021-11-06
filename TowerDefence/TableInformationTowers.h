8ba1316abf2b106f435a3ea331408590866c3f03
Darigraye <66184513+Darigraye@users.noreply.github.com>
2021-11-06 11:43:56 +0300

    map restate on multimap

--- TowerDefence/TableInformationTowers.h
+++ TowerDefence/TableInformationTowers.h
@@ -10,16 +10,6 @@ private:
 	static std::multimap<std::string, info> element;
 
 public:
-	static int getCount(const std::string& nameTower, const int level) { 
-		auto iterator = element.equal_range(nameTower);
-		int returnValue = 0;
-
-		for (auto current = iterator.first; current != iterator.second; ++current) {
-			if (current->second.find(level) != current->second.end()) {
-				returnValue = current->second[level];
-			}
-		}
-
-		return returnValue;
-	}
+	static int getCount(const std::string& nameTower, const int level);
+	
 };
