ebe2e72f3d835c76dda51e4c2c4abe9c63fa6b48
Eugene <eugene.shpachkov@gmail.com>
2021-11-06 11:42:26 +0300

    map reseted on multimap

--- TowerDefence/TableInformationTowers.cpp
+++ TowerDefence/TableInformationTowers.cpp
@@ -1,9 +1,22 @@
 #include "TableInformationTowers.h"
 
-std::map<std::string, info> TableInformationTowers::element =
+std::multimap<std::string, info> TableInformationTowers::element =
 {
 	{ "TowerArchers", { 1, 20 } },
 	{ "TowerArchers", { 2, 35 } },
 	{ "TowerArchers", { 3, 75 } },
 
 };
+
+int TableInformationTowers::getCount(const std::string& nameTower, const int level) {
+	auto iterator = element.equal_range(nameTower);
+	int returnValue = 0;
+
+	for (auto current = iterator.first; current != iterator.second; ++current) {
+		if (current->second.find(level) != current->second.end()) {
+			returnValue = current->second[level];
+		}
+	}
+
+	return returnValue;
+}
--- TowerDefence/TableInformationTowers.h
+++ TowerDefence/TableInformationTowers.h
@@ -7,10 +7,19 @@ using info = std::map<int, int>;
 
 class TableInformationTowers {
 private:
-	static std::map<std::string, info> element;
+	static std::multimap<std::string, info> element;
 
 public:
 	static int getCount(const std::string& nameTower, const int level) { 
-		return element[nameTower][level];
+		auto iterator = element.equal_range(nameTower);
+		int returnValue = 0;
+
+		for (auto current = iterator.first; current != iterator.second; ++current) {
+			if (current->second.find(level) != current->second.end()) {
+				returnValue = current->second[level];
+			}
+		}
+
+		return returnValue;
 	}
 };
